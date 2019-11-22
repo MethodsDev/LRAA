dac80a48322173ca0342b7e6e416d2e122e25baa
Brian Haas <brianjohnhaas@gmail.com>
2019-11-21 21:23:18 -0500

    a start

---
+++ pylib/splice_graph.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys, os, re
+
+
+class splice_graph:
+
+    _read_aln_gap_merge_int = 10
+
+    
+    def __init__(self):
+
+        pass
+
+
+
+
+
+    def set_read_aln_gap_merge(self, read_aln_gap_merge_int):
+
+        self._read_aln_gap_merge_int = read_aln_gap_merge_int
+
+        return
+    
