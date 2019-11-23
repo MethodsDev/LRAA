39a144ccfefddbcc3929af559e26e62a0be2e03f
Brian Haas <brianjohnhaas@gmail.com>
2019-11-22 21:04:21 -0500

    makes draft splice graph

--- pylib/splice_graph.py
+++ pylib/splice_graph.py
@@ -7,8 +7,52 @@ import logging
 import string
 import pysam
 from collections import defaultdict
+import networkx as nx
+
 
 logger = logging.getLogger(__name__)
+
+DEBUG=True
+
+
+class GenomeFeature:
+
+    def __init__(self, lend, rend):
+        self._lend = lend
+        self._rend = rend
+        return
+        
+    def get_coords(self):
+        return(self._lend, self._rend)
+
+    
+class Intron(GenomeFeature):
+
+    intron_id_counter = 0
+    
+    def __init__(self, lend, rend, orient, count):
+        super().__init__(lend, rend)
+        self._orient = orient
+        self._count = count
+
+        Intron.intron_id_counter += 1
+        self._id = "I:{}".format(Intron.intron_id_counter)
+
+        return
+
+class Exon(GenomeFeature):
+
+    exon_id_counter = 0
+
+    def __init__(self, lend, rend, mean_coverage):
+        super().__init__(lend, rend)
+        self._mean_coverage = mean_coverage
+
+        Exon.exon_id_counter += 1
+        self._id = "E:{}".format(Exon.exon_id_counter)
+
+        return
+
 
 
 class splice_graph:
@@ -122,8 +166,6 @@ class splice_graph:
         
         self._contig_seq_str = contig_seq_str
 
-        # print(contig_seq_str)
-        
         return(contig_seq_str)
 
 
@@ -197,12 +239,63 @@ class splice_graph:
         ## do some intron pruning
         self._prune_likely_false_introns()
 
+        ## segment genome coverage
+        exome_segments = self._segment_exome_by_coverage_n_splicing()
+
+        draft_splice_graph = nx.Graph()
+
+        ## add intron nodes.
+        lend_to_intron = defaultdict(list)
+        rend_to_intron = defaultdict(list)
+
+        for intron in self._introns:
+            intron_lend, intron_rend = intron
+            count = self._introns[intron]
+
+            intron_obj = Intron(intron_lend, intron_rend, "?", count)
+            lend_to_intron[intron_lend].append(intron_obj)
+            rend_to_intron[intron_rend].append(intron_obj)
+
+            draft_splice_graph.add_node(intron_obj)
+
+
+        ## add exon nodes and connect to introns.
+        
+        for exon in exome_segments:
+            exon_lend, exon_rend = exon
+
+            exon_mean_cov = self._get_mean_coverage(exon_lend, exon_rend)
+            
+            exon_obj = Exon(exon_lend, exon_rend, exon_mean_cov)
+
+            draft_splice_graph.add_node(exon_obj)
+
+            ## connect to introns where appropriate
+            candidate_splice_left = exon_lend - 1
+            if candidate_splice_left in rend_to_intron:
+                introns = rend_to_intron[candidate_splice_left]
+                for intron_obj in introns:
+                    draft_splice_graph.add_edge(intron_obj, exon_obj)
+
+            candidate_splice_right = exon_rend + 1
+            if candidate_splice_right in lend_to_intron:
+                introns = lend_to_intron[candidate_splice_right]
+                for intron_obj in lend_to_intron[candidate_splice_right]:
+                    draft_splice_graph.add_edge(exon_obj, intron_obj)
+        
+        return draft_splice_graph            
+        
+        
+        
 
     def _prune_likely_false_introns(self):
 
         self._prune_spurious_introns_shared_boundary("left")
         self._prune_spurious_introns_shared_boundary("right")
 
+
+        
+        
     def _prune_spurious_introns_shared_boundary(self, left_or_right):
 
         idx = 0 if left_or_right == "left" else 1
@@ -234,3 +327,118 @@ class splice_graph:
             
         return
 
+    
+    def _prune_weak_splice_neighboring_segments(self):
+
+        #                    I
+        #              _____________
+        #             /             \
+        #   *********/****       ****\************
+        #       A       B         C      D
+        #
+
+        coverage_window_length = 10  # maybe make this configurable.
+        pseudocount = 1
+
+        
+        introns_to_delete = set()
+
+        ofh = None
+        if DEBUG:
+            ofh = open("__splice_neighbor_cov_ratios.dat", 'w')
+        
+        for intron in self._introns:
+            lend, rend = intron
+            intron_abundance = self._introns[intron]
+            
+            A_mean_cov = self._get_mean_coverage(lend - coverage_window_length, lend - 1)
+            B_mean_cov = self._get_mean_coverage(lend, lend + coverage_window_length)
+
+            ratio_B_A = (B_mean_cov + pseudocount)  / (A_mean_cov + pseudocount)
+                        
+            C_mean_cov = self._get_mean_coverage(rend - coverage_window_length, rend)
+            D_mean_cov = self._get_mean_coverage(rend + 1, rend + coverage_window_length)
+            
+            ratio_C_D = (C_mean_cov + pseudocount) / (D_mean_cov + pseudocount)
+
+            if DEBUG:
+                ofh.write("{}".format(lend) +
+                          "\tI:{}".format(intron_abundance) +
+                          "\tA:{:.3f}".format(A_mean_cov) +
+                          "\tB:{:.3f}".format(B_mean_cov) +
+                          "\tB/A:{:.3f}".format(ratio_B_A) +
+                          
+                          "\n{}".format(rend) +
+                          "\tC:{:.3f}".format(C_mean_cov) +
+                          "\tD:{:.3f}".format(D_mean_cov) +
+                          "\tC/D:{:.3f}".format(ratio_C_D) )
+            
+        return
+
+
+    
+    def _get_mean_coverage(self, start, end):
+
+        cov_sum = 0
+        cov_len = 0
+        
+        for i in range(start, end+1):
+            if i>= 0 and i < len(self._contig_base_cov):
+                cov_len += 1
+                cov_sum += self._contig_base_cov[i]
+
+        if cov_len > 0:
+            mean_cov = cov_sum / cov_len
+            return mean_cov
+        else:
+            logger.warning("coverage requested to position {} extends beyond length of contig {}".format(end, self._contig_acc))
+            return 0
+        
+
+
+    def _segment_exome_by_coverage_n_splicing(self):
+
+
+        left_splice_sites = set()
+        right_splice_sites = set()
+
+        for intron in self._introns:
+            lend, rend = intron
+            left_splice_sites.add(lend)
+            right_splice_sites.add(rend)
+
+
+        exome_segments = list()
+        exon_seg_start = None
+        for i in range(0, len(self._contig_base_cov)):
+            if exon_seg_start is None:
+                if self._contig_base_cov[i]:
+                    # start new exome segment
+                    exon_seg_start = i
+            else:
+                # handle current segment
+                if self._contig_base_cov[i] == 0:
+                    # stop segment, add to seg list
+                    exome_segments.append([exon_seg_start, i-1])
+                    exon_seg_start = None
+                elif i in left_splice_sites:
+                    exome_segments.append([exon_seg_start, i-1])
+                    exon_seg_start = i
+                elif i in right_splice_sites:
+                    exome_segments.append([exon_seg_start, i])
+                    exon_seg_start = None
+
+
+        # get last one if it runs off to the end of the contig
+        if exon_seg_start is not None:
+            exome_segments.append([exon_seg_start, len(self._contig_base_cov)-1])
+
+
+        if DEBUG:
+            # write exon list to file
+            with open("__exome_regions.bed", 'w') as ofh:
+                for segment in exome_segments:
+                    ofh.write("\t".join([self._contig_acc, str(segment[0]-1), str(segment[1]+1)]) + "\n")
+
+        return exome_segments
+    
