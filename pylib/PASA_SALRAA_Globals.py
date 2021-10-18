fde807c5005e814e12390f37965eedb05d3d268d
Brian Haas <brianjohnhaas@gmail.com>
2021-10-18 11:33:33 -0400

    update

--- pylib/PASA_SALRAA_Globals.py
+++ pylib/PASA_SALRAA_Globals.py
@@ -8,7 +8,7 @@ DEBUG=False
 config = {
     # min per_id 
     'min_per_id_illumina' : 98,
-    'min_per_id_pacbio' : 80,
+    'min_per_id_pacbio' : 1,
 
     # weights
     'weight_short_read' : 1,
